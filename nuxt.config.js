da878181eb3812f806991acdb8d1b2c96f68f432
Camilo García <gcgarciab@slabcode.com>
2019-05-17 16:10:48 -0500

    Deply Nuxt - Netlify

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,13 @@ import pkg from './package'
 
 export default {
   mode: 'universal',
-
+  generate: {
+    routes: [
+      '/posts/balut',
+      '/posts/whereIsIt',
+      '/posts/how'
+    ]
+  },
   /*
   ** Headers of the page
   */
